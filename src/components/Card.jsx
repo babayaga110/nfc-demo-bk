3b5387535706693f8ee576d411c8e45fb7b074ef
Karan Pandit <70019018+babayaga110@users.noreply.github.com>
2024-11-25 12:13:11 +0530

    new changes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -35,31 +35,35 @@ export default function Card({
       </div>
       <div className="flex flex-col w-1/4">
         {data?.show ? (
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white flex justify-center items-center flex-1"
-            onClick={() => handleReset(doctor)}
-          >
-            <Minus />
-          </button>
-        ) : (
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white flex justify-center items-center flex-1"
-            onClick={() => handleSet(doctor)}
-          >
-            <Plus />
-          </button>
-        )}
+          data?.image === doctor?.image ? (
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white flex justify-center items-center flex-1"
+              onClick={() => handleReset(doctor)}
+            >
+              <Minus />
+            </button>
+          ) : (
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white flex justify-center items-center flex-1"
+              onClick={() => handleSet(doctor)}
+            >
+              <Plus />
+            </button>
+          )
+        ) : null}
         {data?.show ? (
-          <label class="inline-flex items-center cursor-pointer flex-1 w-[100%]">
-            <input
-              type="checkbox"
-              value=""
-              class="sr-only peer"
-              onChange={(e) => handleAvailable(e.target.checked)}
-              checked={data?.available}
-            />
-            <div class="relative w-full h-full bg-red-400 peer-focus:outline-none dark:peer-focus:ring-blue-800  peer dark:bg-red-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[10%] after:start-[5%] after:bg-white after:border-gray-300 after:border after:rounded-md  after:h-[80%] after:w-[45%] after:transition-all dark:border-gray-600 peer-checked:bg-green-400"></div>
-          </label>
+          data?.image === doctor?.image ? (
+            <label class="inline-flex items-center cursor-pointer flex-1 w-[100%]">
+              <input
+                type="checkbox"
+                value=""
+                class="sr-only peer"
+                onChange={(e) => handleAvailable(e.target.checked)}
+                checked={data?.available}
+              />
+              <div class="relative w-full h-full bg-red-400 peer-focus:outline-none dark:peer-focus:ring-blue-800  peer dark:bg-red-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[10%] after:start-[5%] after:bg-white after:border-gray-300 after:border after:rounded-md  after:h-[80%] after:w-[45%] after:transition-all dark:border-gray-600 peer-checked:bg-green-400"></div>
+            </label>
+          ) : null
         ) : null}
         <button
           className="bg-orange-400 hover:bg-orange-600 text-white flex justify-center items-center flex-1"
